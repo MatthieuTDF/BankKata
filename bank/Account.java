bbf715a6221e4ad761e0baaeea2e960c9bef41ef
Eduard Serban <serbanedi.fr@gmail.com>
2020-05-08 13:01:48 +0200

    1

--- bank/Account.java
+++ bank/Account.java
@@ -4,15 +4,67 @@ class Account {
 
     // Attributes
     // TODO
+    private String fullName;
+    private Integer amount;
+    private Integer roof;
+    private Boolean isBlocked;
 
     // Constructor
     // TODO
+    public Account(String fullName, Integer amount, Integer roof, Boolean isBlocked){
+        this.fullName = fullName;
+        this.amount = amount;
+        this.roof = roof;
+        this.isBlocked = isBlocked;
+    }
 
     // Methods
     // TODO
 
+    //Les getteurs et setter je les ai importé depuis l'option generate
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer getRoof() {
+        return roof;
+    }
+
+    public void setRoof(Integer roof) {
+        this.roof = roof;
+    }
+
+    public Boolean getBlocked() {
+        return isBlocked;
+    }
+
+    public void setBlocked(Boolean blocked) {
+        isBlocked = blocked;
+    }
+    //Getter setter fin
+    public void wireAmount(Integer wire){
+        int total = amount + wire;
+        if (total <= roof){
+            this.amount = total;
+        }
+    }
+
+
+
     public String toString() {
         // TODO
-        return "";
+        return "Nom complet: " + this.fullName + "; Solde du compte: " + this.amount + "; Plafond de virements: " + this.roof + "; Compte bloqué: " + this.isBlocked;
     }
 }
--- bank/Bank.java
+++ bank/Bank.java
@@ -2,9 +2,12 @@ package bank;
 
 
 import java.sql.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Bank {
 
+    private static final String TABLE_NAME = "accounts";
     private static final String JDBC_DRIVER = "org.postgresql.Driver";
     private static final String DB_URL = "jdbc:postgresql://localhost:5439/postgres";
     private static final String DB_USER = "postgres";
@@ -14,6 +17,9 @@ public class Bank {
 
     public Bank() {
         initDb();
+        if (!checkDataBase()){
+            createDataBase();
+        }
     }
 
     private void initDb() {
@@ -22,11 +28,40 @@ public class Bank {
             c = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
             System.out.println("Opened database successfully");
 
-            // TODO Init DB
-
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
+        }
+    }
+
+    private boolean checkDataBase(){
+        String q = "SELECT EXISTS (" +
+                "SELECT 1 " +
+                "FROM pg_tables " +
+                "WHERE schemaname = 'public'" +
+                "AND tablename = '" + TABLE_NAME + "')" ;
+
+        try (PreparedStatement s = c.prepareStatement(q)) {
+            ResultSet r = s.executeQuery();
+            r.next();
+            return r.getBoolean(1);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    private void createDataBase(){
+        try (Statement s = c.createStatement()) {
+            s.executeUpdate("CREATE TABLE " + TABLE_NAME + "(" +
+                    "name VARCHAR(120) NOT NULL," +
+                    "balance INT NOT NULL," +
+                    "threshold INT NOT NULL," +
+                    "locked BOOLEAN NOT NULL DEFAULT false, " +
+                    "PRIMARY KEY (name))");
+            System.out.println("Table 'accounts' created successfully");
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
     }
 
@@ -53,6 +88,8 @@ public class Bank {
 
     public void createNewAccount(String name, int balance, int threshold) {
         // TODO
+
+
     }
 
     public String printAllAccounts() {
